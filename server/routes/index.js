e4992d28d2f0afa4dac735b04af4f11a7ae35757
asarioglo <alexandr.sarioglo@geotoolkit.net>
2017-09-06 18:15:45 -0500

    Initial Commit

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/name', function(req, res, next){
+	res.json({"name": "Sasha"});
+});
+
+module.exports = router;
