dd945b0d29bdde2ed7c862effaaffa41dae00de9
asarioglo <sasha@geotoolkit.net>
2017-09-07 00:33:39 -0500

    Added some test components to test routing and bootstrap libs

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  private name: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    let body = this.http.get("http://localhost:3000/api/name",{});
+    body.subscribe((res: any) => {
+      this.name = res.name;
+    })
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {HttpClientModule} from "@angular/common/http";
+import { TestComponent } from './test/test.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {RouterModule} from "@angular/router";
+import { MainPageComponent } from './main-page/main-page.component';
+
+const appRoutes = [
+  {path: "", component: MainPageComponent},
+  {path: "test", component: TestComponent},
+  {path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    PageNotFoundComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
