b6e811ab3b5bb4a261f8fefc95b8153fa02fae9e
asarioglo <asarioglo@gmail.com>
2017-09-19 00:10:43 -0500

    Added a button to deploy the asarioglo.com automatically from the webpage

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public alerts: Array<any> = [];
+
+  public deploy() {
+    this.updateAlerts("info", "Sending a request to pull, build, and deploy the code to asarioglo.com");
+    // TODO: this address must become global
+    let body = this.http.get("http://185.179.190.61/api/control/sasha_only/remote/deploy");
+    body.subscribe(
+      data => {
+        this.updateAlerts("success", "Deploy sucessful, refresh the page to apply the changes");
+      },
+      err => {
+        this.updateAlerts("danger", "Request failed, check your console");
+        console.log(err);
+      }
+    )
+  }
+
+  private updateAlerts(type, message) {
+    this.alerts = [];
+    this.alerts.push({
+      type: type,
+      text: message
+    });
+  }
 
   ngOnInit() {
   }
